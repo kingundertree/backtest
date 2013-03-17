f8b7a8654af1d53a9f3397ac2c60ad454a09d9d8
kingundertree <kingundertree@163.com>
2013-03-17 10:29:04 +0800

    合并代码
    合并代码，查看效果

--- addresslist(6)/js/commom.js
+++ addresslist(6)/js/commom.js
@@ -1,21 +1,30 @@
+
+function showMenu(){
+	var sl=document.getElementById('mycustomer');
+	var slLeft=sl.offsetLeft;
+
+	if (slLeft==0) {
+		sl.style.left="240px";
+		//sl.style.boxshadow="-3px 0 4px #999";
+	}else{
+		sl.style.left="0px";
+	}
+}
+
+
 angular.module('customerlistall',[]).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/temple', {templateUrl: 'temple/customerlistall.html', controller: customerlist}).
-      when('/temple/:extra', {templateUrl: 'temple/userdetail.html', controller: detail}).
       otherwise({redirectTo: '/temple'});
 }]);
 
-function detail($scope, $routeParams) {
-	$scope.extra = $routeParams.extra;
-}
-detail.$inject = ['$scope', '$routeParams'];
 
 function customerlist($scope, $http) {
 	$http.get("data/user.json").success(function(data) {
-		
-		$scope.orderProp = '-lastcalltime';
 
+		// 最后来电时间
+		$scope.orderProp = "-lastcalltime";
 		for (var i in data) {
 			var time = data[i].lastcalltime;
 			var now = new Date().getTime();  
@@ -39,23 +48,7 @@ function customerlist($scope, $http) {
 
 	$scope.phone = function(user) {
 		document.getElementById("cover").style.display = "block";
-		$scope.user = user;}
-
-	$scope.phoneCancle = function() {
-		document.getElementById("cover").style.display = "none";}
-
-	$scope.showMenu=function(){
-		var sl=document.getElementById('mycustomer');
-		var slLeft=sl.offsetLeft;
-
-		if (slLeft==0) {
-			sl.style.left="240px";
-			//sl.style.boxshadow="-3px 0 4px #999";
-		}else{
-			sl.style.left="0px";
-		}
+		$scope.user = user;
 	}
 }
-
-
 
